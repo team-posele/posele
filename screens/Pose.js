59f6650945216df2fc4da95b5693594c1e607b28
ty.monaghan <ty.monaghan@gmail.com>
2022-01-31 01:05:33 -0600

    add styling to warning screen ("pose")

--- screens/Pose.js
+++ screens/Pose.js
@@ -1,17 +1,20 @@
+import {Alert, Button, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View, Image, Button, Alert} from 'react-native';
-import {TouchableOpacity} from 'react-native-web';
 
 export default function Pose() {
   return (
     <View style={styles.container}>
       <View style={styles.instructionsView}>
-        <Text style={styles.instructionsText}>Playing Posele:</Text>
-        <Text style={styles.instructionsText}>When you click Pose Now, you will see an image</Text>
-        <Text style={styles.instructionsText}>Your device camera will start</Text>
+        <Text style={[styles.instructionsText, {textAlign: 'center'}]}>Playing Posele:</Text>
         <Text style={styles.instructionsText}>
-          You have five seconds to line up your shot and match the pose in the image.
+          1. When you click Pose Now, you will see an image
         </Text>
+        <Text style={styles.instructionsText}>2. Your device camera will start</Text>
+        <Text style={styles.instructionsText}>
+          3. You have five seconds to line up your shot and match the pose in the image.
+        </Text>
+        <Text style={styles.instructionsText}>Good luck! </Text>
       </View>
       <View style={styles.pose}>
         <Image style={styles.image} source={require('../assets/cameraWarning.jpg')}></Image>
@@ -21,6 +24,9 @@ export default function Pose() {
         </Text>
       </View>
       <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button, styles.smallButton]}>
+          <Text style={[styles.buttonText, styles.smallButtonText]}>Back</Text>
+        </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Pose Now!</Text>
         </TouchableOpacity>
@@ -38,36 +44,58 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   instructionsView: {
-    flex: 1,
-    marginTop: 15,
+    flex: 2,
+    marginTop: 30,
     alignContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginHorizontal: 10,
   },
   instructionsText: {
-    color: 'darkgray',
+    backgroundColor: '#414BB2',
+    color: 'white',
+    fontSize: 20,
+    padding: 10,
   },
-  pose: {flex: 3},
+  pose: {flex: 2, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'},
   warning: {
     color: 'red',
     textAlign: 'center',
+    width: '50%',
+    fontSize: 20,
+    margin: 5,
   },
   image: {
     resizeMode: 'contain',
-    width: 125,
-    height: 125,
+    width: '35%',
+    margin: 5,
   },
   button: {
     backgroundColor: '#414BB2',
-    width: '70%',
+    width: '60%',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 5,
   },
   buttonText: {
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
   },
-  buttonContainer: {flex: 2, width: '100%', alignItems: 'center'},
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'flex-start',
+  },
+  smallButton: {
+    width: '25%',
+    backgroundColor: 'white',
+    borderColor: '#414BB2',
+    borderWidth: 1,
+  },
+  smallButtonText: {color: '#414BB2'},
 });
