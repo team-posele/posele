f989b1f17e188d1d31126c49686904892ffc9f11
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-30 20:27:14 -0600

    login and signup appear to be working

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,48 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {KeyboardAvoidingView, TextInput, TouchableOpacity} from 'react-native-web';
+import {auth} from '../firebase';
+import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
+  const navigate = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigate.navigate('MyTabs');
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const handleSignup = () => {
+    console.log(`you signed up`);
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log(`Signing up with user email: ${user.email}`);
+      })
+      .catch(error => {
+        console.log(`error! ${error}`);
+      });
+  };
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log(`Logging in with user email: ${user.email}`);
+      })
+      .catch(error => {
+        console.log(`error! ${error}`);
+      });
+  };
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
@@ -27,10 +65,20 @@ const Login = () => {
           value={password}
           onChangeText={text => setPassword(text)}
         />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
       </View>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.buttonText} onPress={handleLogin}>
+          Login
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.signupButton}>
+        <Text style={styles.signupButtonText} onPress={handleSignup}>
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.signupButton}>
+        <Text style={styles.signupButtonText}>Play as Guest</Text>
+      </TouchableOpacity>
     </KeyboardAvoidingView>
   );
 };
@@ -44,17 +92,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  // headerContainer: {flex: 1},
-  headerText: {fontSize: 30, textAlign: 'center'},
-  button: {
+  headerText: {fontSize: 24, textAlign: 'center'},
+  loginButton: {
     backgroundColor: '#414BB2',
     width: '60%',
-    padding: 15,
+    padding: 10,
     borderRadius: 10,
     alignItems: 'center',
+    marginTop: 20,
+  },
+  signupButton: {
+    backgroundColor: 'white',
+    borderColor: '#414BB2',
+    borderWidth: 3,
+    width: '60%',
+    padding: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20,
   },
   buttonText: {
     color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  signupButtonText: {
+    color: '#414BB2',
     fontSize: 16,
     fontWeight: 'bold',
   },
